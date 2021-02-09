b7aaef13171971ec4615c578ad45c127db30c04e
Daniel Bjorn <danielbjorn94@gmail.com>
2021-02-09 20:03:00 +0000

    updated css3 and added crypto feed to home#index as temp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 
 class HomeController < ApplicationController
+
   def index
+
+
+
+  	# ----------------- THIS CODE BELONGS TO CRYPTO CURRENCY API ---------------------
+  	
+  	require 'net/http'
+  	require 'json'
+
+  	@url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+  	@uri = URI(@url)
+  	@response = Net::HTTP.get(@uri)
+  	@news = JSON.parse(@response)
+
+
+  	# ----------------- THIS CODE BELONGS TO CRYPTO CURRENCY API ---------------------
+
   end
+
+
 end
