ab31bd4935032953e0703c7e7972f3201cd8de38
Daniel Bjorn <danielbjorn94@gmail.com>
2021-02-16 20:58:54 +0000

    added some new logos and crypto api

--- app/controllers/crypto_controller.rb
+++ app/controllers/crypto_controller.rb
@@ -5,10 +5,20 @@ class CryptoController < ApplicationController
 		require 'net/http'
 	  	require 'json'
 
+
+	  	#Grabs news data
 	  	@url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
 	  	@uri = URI(@url)
 	  	@response = Net::HTTP.get(@uri)
 	  	@news = JSON.parse(@response)
+
+	  	#gets news data
+	  	@prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,TRX,DOT,XRP,XLM&tsyms=USD'
+	  	@prices_uri = URI(@prices_url)
+	  	@prices_response = Net::HTTP.get(@prices_uri)
+	  	@prices = JSON.parse(@prices_response)
+
+
 
 	end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,6 @@ class HomeController < ApplicationController
 
   def index
 
-
-
-  	# ----------------- THIS CODE BELONGS TO CRYPTO CURRENCY API ---------------------
-  	
-
-  	# ----------------- THIS CODE BELONGS TO CRYPTO CURRENCY API ---------------------
-
   end
 
 
